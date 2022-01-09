8d3df85b873184307e24a222571eb5e26cbcce64
Wickette <stephany.clark2@gmail.com>
2022-01-09 10:14:11 -0500

    updated header and card component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -57,15 +57,15 @@ export default function App() {
       />
       <div
         className='p-5 text-center bg-image'
-        style={{ height: '65rem' }}
+        style={{ height: '62rem' }}
       >
         <div className='mask'>
           <div className='d-flex justify-content-center align-items-center h-100'>
             <div className='text-white'>
               <img style={{height:'100px'}} src={logo} alt='logo' className='mb-6'></img>
-              <h4 style={{fontStyle:'italic'}} className='mb-5'>Take a break from your busy schedule, with "A Touch of Space" content</h4>
-              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
-                Check out the Weather on Mars
+              <h4 style={{fontStyle:'italic'}} className='mb-5'>Take a quick break from your busy day and learn a bit about space</h4>
+              <a style={{color: 'white', border:'#FFF9A6 solid 1px'}} className='btn btn-lg mars-info' href='#POD' role='button'>
+                Jump to NASA's picture of the day
               </a>
             </div>
           </div>
--- src/components/POD/POD.jsx
+++ src/components/POD/POD.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBRipple } from 'mdb-react-ui-kit';
 import { LikeButton } from '@lyket/react';
 import './pod.css'
 
@@ -15,32 +14,34 @@ export default function UsersData() {
         fetchData(res)
       })
   }
-
   useEffect(() => {
     getData()
   }, [])
 
   return (
     <>
-      <MDBCard style={{ maxWidth: '50rem'}}>
-      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-        <MDBCardImage src={Data.url} fluid alt='...' />
-        <a>
-          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-        </a>
-      </MDBRipple>
-      <MDBCardBody>
-        <MDBCardTitle>{Data.title}</MDBCardTitle>
-        <MDBCardText>
-          {Data.explanation}
-        </MDBCardText>
-        <LikeButton
-          namespace="my-blog-post"
-          id="how-to-beat-me-at-chess"
-          component={LikeButton.templates.Twitter}
-        />
-      </MDBCardBody>
-    </MDBCard>
+      <div id='POD' className="card" style={{width: '100rem'}}>
+        <div className="row no-gutters">
+          <div className="col-md-7">
+            <img src={Data.url} alt="nasa" />
+            <rect width="100%" height="100%" fill="#868e96" />
+          </div>
+          <div style={{margin: 'auto 0'}} className="col-md-5">
+            <div className="card-body">
+              <h5 style={{marginBottom: '6rem'}} className="card-title">{Data.title}</h5>
+              <p className="card-text">
+                {Data.explanation}
+              </p>
+              <LikeButton
+            component={LikeButton.templates.Twitter}
+          />
+            </div>
+          </div>
+        </div>
+    </div>
+       
+
+   
     </>
   )
 }
